de6590c57a5f5712b52d686caefa6bc8fb4f06fc
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-10-15 18:49:02 -0400

    first commit

---
+++ src/Quiz.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+let quizData = require('./quiz-data.json')
+
+class Quiz extends Component {
+  render() {
+    return <div>Quiz</div>
+  }
+}
+
+export default Quiz
