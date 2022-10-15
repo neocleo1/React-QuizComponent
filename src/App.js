043de480545cd3a24fd14982f5dd3e4c28888413
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-10-15 19:30:01 -0400

    imported in app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
+import './Quiz'
+import Quiz from './Quiz'
 
 class App extends Component {
   render() {
-    return (
-      <div />
-    )
+    return <Quiz />
   }
 }
 
