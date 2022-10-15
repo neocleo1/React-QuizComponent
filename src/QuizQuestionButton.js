416c52ea0beb873262dd104b9dd352921654879a
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-10-15 19:56:09 -0400

    testing module3

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+
+class QuizQuestionButton extends Component {
+  render() {
+    return (
+      <li>
+        <button>{button_text}</button>
+      </li>
+    )
+  }
+}
+
+export default QuizQuestionButton
