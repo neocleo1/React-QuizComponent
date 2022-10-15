dbe3b66c87c550c3f6f54ce6413adc5953a3e4a2
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-10-15 19:50:54 -0400

    testing module2

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,9 @@ class Quiz extends Component {
   render() {
     return (
       <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[-1]} />
+        <QuizQuestion
+          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+        />
       </div>
     )
   }
